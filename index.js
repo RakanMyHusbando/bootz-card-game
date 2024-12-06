1a20b44c0916ce8cae9b8a2818f31e47366f1a01
RakanMyHusbando <hellwing.jonas@web.de>
2024-12-06 15:53:18 +0100

    add post user card

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
-import random from "./random.json"
-
-interface User {
-    id: number | null;
-    name: string;
-    discord_id: string;
-    user_cards: number[] | null;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function ranInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-interface Card {
-    id: number | null;
-    title: string;
-    description: string;
-    type: string;
-    rarity: number;
-    cost: number;
-    attack: number;
-    defense: number;
-    health: number;
-}
-
-function ranInt(min: number, max: number):number {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min + 1)) + min
-}
-
-
 // post 100 test cards
-let j = 0
+/*let j = 0
 for(let i = 0; i < random.names.length; i++){
     const card: Card = {
         id: null,
@@ -66,13 +45,28 @@ for(let i = 0; i < 100; i++){
         },
         body: JSON.stringify(user)
     })
-}
-
+}*/
 // post 100 cards to random users
-for(let i = 0; i < 100; i++){
-    const user_id = ranInt(1, 100)
-    const card_id = ranInt(1, 100)
-    fetch(`http://localhost:5000/user/${user_id}/card/${card_id}`, {
+for (var i = 0; i < 100; i++) {
+    var user_id = ranInt(1, 100);
+    var card_id = ranInt(1, 100);
+    fetch("http://localhost:5000/user/".concat(user_id, "/card/").concat(card_id), {
         method: "POST"
     })
+        .then(function (res) { return res.json(); })
+        .then(function (data) { return console.log(data); });
 }
+var user = [];
+var card = [];
+// get all cards
+fetch("http://localhost:5000/card")
+    .then(function (res) { return res.json(); })
+    .then(function (data) {
+    card.push.apply(card, data);
+});
+// get all users
+fetch("http://localhost:5000/user")
+    .then(function (res) { return res.json(); })
+    .then(function (data) {
+    user.push.apply(user, data);
+});
