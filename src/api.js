ef4078ce9ae1e0e786e01f8bbfb60bdc26912e98
RakanMyHusbando <hellwing.jonas@web.de>
2024-12-08 13:57:46 +0100

    add unknown card amount too user

--- src/api.js
+++ src/api.js
@@ -1,30 +1,37 @@
-import { Storage } from "./storage.js"
+import { Storage } from "./storage.js";
 
 const formApiResponse = (res, status, data, message) => {
-    console.log(message)
+    console.log(message);
     const body = {
         status: status,
         data: data,
-        message: message.toString()
-    }
-    res.status(status)
-    res.json(body)
-}
+        message: message.toString(),
+    };
+    res.status(status);
+    res.json(body);
+};
 
 export class ApiHandler extends Storage {
-
     /**
      * Creates an instance of ApiHandler.
-     * @param {string} dbFile - The path to the SQLite database file. 
+     * @param {string} dbFile - The path to the SQLite database file.
      * @param {number} [timeout=5000] - The timeout for the SQLite database connection.
      */
     constructor(dbFile, timeout = 5000) {
-        super(dbFile, timeout)
+        super(dbFile, timeout);
     }
 
-/* -------------------------------------------- Card Handler -------------------------------------------- */
+    /* -------------------------------------------- Card Handler -------------------------------------------- */
 
-    #cardKeys = ["title","description","type","rarity","attack","defense","health"]
+    #cardKeys = [
+        "title",
+        "description",
+        "type",
+        "rarity",
+        "attack",
+        "defense",
+        "health",
+    ];
 
     #StorageToRespData = (storageData) => {
         return {
@@ -35,9 +42,9 @@ export class ApiHandler extends Storage {
             rarity: storageData.rarity,
             attack: storageData.attack,
             defense: storageData.defense,
-            health: storageData.health
-        }
-    }
+            health: storageData.health,
+        };
+    };
 
     /**
      * Handle POST request to create a new card.
@@ -56,17 +63,16 @@ export class ApiHandler extends Storage {
      * @returns {null}
      */
     CardPost(req, res) {
-        const data = []
+        const data = [];
         try {
             for (const key of this.#cardKeys) {
-                if (!req.body[key]) 
-                    throw new Error("Missing required fields") 
-                data.push(req.body[key])
-            }   
-            this.insert("card", this.#cardKeys, data)
-            formApiResponse(res, 200, null, "Card created")
-        } catch(err) {
-            return formApiResponse(res, 500, null, err)
+                if (!req.body[key]) throw new Error("Missing required fields");
+                data.push(req.body[key]);
+            }
+            this.insert("card", this.#cardKeys, data);
+            formApiResponse(res, 200, null, "Card created");
+        } catch (err) {
+            return formApiResponse(res, 500, null, err);
         }
     }
 
@@ -76,16 +82,16 @@ export class ApiHandler extends Storage {
      * @param {Object} res - The response object.
      * @returns {null}
      */
-    CardGetAll(req,res) {
-        const data = []
+    CardGetAll(req, res) {
+        const data = [];
         try {
-            const rows = this.select("card",["*"])
-            rows.forEach(row => data.push(this.#StorageToRespData(row)))
-            if (data.length === 0) 
-                return formApiResponse(res, 404, null, "No cards found")
-            formApiResponse(res, 200, data, "Card retrieved")
-        } catch(err) {
-            formApiResponse(res, 500, null, err)
+            const rows = this.select("card", ["*"]);
+            rows.forEach((row) => data.push(this.#StorageToRespData(row)));
+            if (data.length === 0)
+                return formApiResponse(res, 404, null, "No cards found");
+            formApiResponse(res, 200, data, "Card retrieved");
+        } catch (err) {
+            formApiResponse(res, 500, null, err);
         }
     }
 
@@ -98,19 +104,22 @@ export class ApiHandler extends Storage {
      * @returns {null}
      */
     CardGetById(req, res) {
-        const data = []
+        const data = [];
         try {
-            const rows = this.select("card",["*"],{id:parseInt(req.params.id)})
-            rows.forEach(storageData => data.push(this.#StorageToRespData(storageData)))
-            if (data.length === 0) 
-                return formApiResponse(res, 404, null, "No cards found")
-            formApiResponse(res, 200, data, "Card retrieved")
-        } catch(err) {
-            formApiResponse(res, 500, null, err)
+            const rows = this.select("card", ["*"], {
+                id: parseInt(req.params.id),
+            });
+            rows.forEach((storageData) =>
+                data.push(this.#StorageToRespData(storageData)),
+            );
+            if (data.length === 0)
+                return formApiResponse(res, 404, null, "No cards found");
+            formApiResponse(res, 200, data, "Card retrieved");
+        } catch (err) {
+            formApiResponse(res, 500, null, err);
         }
     }
 
-  
     /**
      * Handle PATCH request to update a card by id.
      * @param {Object} req - The request object.
@@ -128,18 +137,20 @@ export class ApiHandler extends Storage {
      * @returns {null}
      */
     CardPatch(req, res) {
-        const columns = []
-        const values = []
+        const columns = [];
+        const values = [];
         try {
-            for (const key of this.#cardKeys) 
+            for (const key of this.#cardKeys)
                 if (req.body[key]) {
-                    values.push(req.body[key])
-                    columns.push(key)
+                    values.push(req.body[key]);
+                    columns.push(key);
                 }
-            this.update("card", columns, values, {id:parseInt(req.params.id)})
-            formApiResponse(res, 200, null, "Card updated")
-        } catch(err) {
-            formApiResponse(res, 500, null, err)
+            this.update("card", columns, values, {
+                id: parseInt(req.params.id),
+            });
+            formApiResponse(res, 200, null, "Card updated");
+        } catch (err) {
+            formApiResponse(res, 500, null, err);
         }
     }
 
@@ -153,15 +164,15 @@ export class ApiHandler extends Storage {
      */
     CardDelete(req, res) {
         try {
-            this.delete("card", {id:parseInt(req.params.id)})
-            formApiResponse(res, 200, data, "Card deleted")
-        } catch(err) {
-            formApiResponse(res, 500, null, err)
+            this.delete("card", { id: parseInt(req.params.id) });
+            formApiResponse(res, 200, data, "Card deleted");
+        } catch (err) {
+            formApiResponse(res, 500, null, err);
         }
     }
-/* -------------------------------------------- User Handler -------------------------------------------- */
+    /* -------------------------------------------- User Handler -------------------------------------------- */
 
-    #userKeys = ["name","discord_id"]   
+    #userKeys = ["name", "discord_id"];
 
     /**
      * Handle POST request to create a new user.
@@ -173,17 +184,21 @@ export class ApiHandler extends Storage {
      * @returns {null}
      */
     UserPost(req, res) {
-        const data = []
+        const data = [];
         try {
             for (const key of this.#userKeys) {
-                if (!req.body[key]) 
-                    throw new Error("Missing required fields")
-                data.push(req.body[key])      
+                if (!req.body[key]) throw new Error("Missing required fields");
+                data.push(req.body[key]);
             }
-            this.insert("user", this.#userKeys, data)
-            formApiResponse(res, 200, null, "User created")
-        } catch(err) {
-            formApiResponse(res, 500, null, err)
+            data.push(0);
+            this.insert(
+                "user",
+                this.#userKeys.concat("unknown_card_amount"),
+                data,
+            );
+            formApiResponse(res, 200, null, "User created");
+        } catch (err) {
+            formApiResponse(res, 500, null, err);
         }
     }
 
@@ -193,19 +208,18 @@ export class ApiHandler extends Storage {
      * @param {Object} res - The response object.
      * @returns {null}
      */
-    UserGetAll(req,res) {
+    UserGetAll(req, res) {
         try {
-            const rows = this.select("user",["*"])
-            if(rows.length > 0){
+            const rows = this.select("user", ["*"]);
+            if (rows.length > 0) {
                 for (let i = 0; i < rows.length; i++) {
-                    rows[i].cards = this.#UserCardGet(rows[i].id)
+                    rows[i].cards = this.#UserCardGet(rows[i].id);
                 }
-                formApiResponse(res, 200, rows, "User retrieved")
-            } else
-                formApiResponse(res, 404, null, "No user found")
-        } catch(err) {
-            formApiResponse(res, 500, null, err)
-        } 
+                formApiResponse(res, 200, rows, "User retrieved");
+            } else formApiResponse(res, 404, null, "No user found");
+        } catch (err) {
+            formApiResponse(res, 500, null, err);
+        }
     }
 
     /**
@@ -216,16 +230,17 @@ export class ApiHandler extends Storage {
      * @param {Object} res - The response object.
      * @returns {null}
      **/
-    UserGetById(req,res) { 
+    UserGetById(req, res) {
         try {
-            const rows = this.select("user",["*"],{id:parseInt(req.params.id)})
-            if(rows.length == 1){
-                rows[0].cards = this.#UserCardGet(parseInt(req.params.id))
-                formApiResponse(res, 200, rows, "User retrieved")
-            } else
-                formApiResponse(res, 404, null, "No user found")
-        } catch(err) {
-            formApiResponse(res, 500, null, err)
+            const rows = this.select("user", ["*"], {
+                id: parseInt(req.params.id),
+            });
+            if (rows.length == 1) {
+                rows[0].cards = this.#UserCardGet(parseInt(req.params.id));
+                formApiResponse(res, 200, rows, "User retrieved");
+            } else formApiResponse(res, 404, null, "No user found");
+        } catch (err) {
+            formApiResponse(res, 500, null, err);
         }
     }
 
@@ -240,16 +255,17 @@ export class ApiHandler extends Storage {
      * @param {Object} res - The response object.
      * @returns {null}
      */
-    UserPatch(req,res) {
-        const data = []
+    UserPatch(req, res) {
+        const data = [];
         try {
-            for (const key of this.#userKeys) 
-                if (req.body[key]) 
-                    data.push(req.body[key])
-            this.update("user", this.#userKeys, values, {id:parseInt(req.params.id)})
-            formApiResponse(res, 200, null, "User updated")
-        } catch(err) {
-            formApiResponse(res, 500, null, err)
+            for (const key of this.#userKeys)
+                if (req.body[key]) data.push(req.body[key]);
+            this.update("user", this.#userKeys, values, {
+                id: parseInt(req.params.id),
+            });
+            formApiResponse(res, 200, null, "User updated");
+        } catch (err) {
+            formApiResponse(res, 500, null, err);
         }
     }
 
@@ -261,19 +277,22 @@ export class ApiHandler extends Storage {
      * @param {Object} res - The response object.
      * @returns {null}
      */
-    UserDelete(req,res) {
+    UserDelete(req, res) {
         try {
-            this.#UserCardGet(req.params.id).forEach(
-                card => this.delete("user_card",{user_id:parseInt(req.params.id),card_id:card.id})
-            )
-            this.delete("user", {id:parseInt(req.params.id)})
-            formApiResponse(res, 200, null, "User deleted")
-        } catch(err) {
-            formApiResponse(res, 500, null, err)
+            this.#UserCardGet(req.params.id).forEach((card) =>
+                this.delete("user_card", {
+                    user_id: parseInt(req.params.id),
+                    card_id: card.id,
+                }),
+            );
+            this.delete("user", { id: parseInt(req.params.id) });
+            formApiResponse(res, 200, null, "User deleted");
+        } catch (err) {
+            formApiResponse(res, 500, null, err);
         }
     }
 
-/* -------------------------------------------- User-Card Handler -------------------------------------------- */
+    /* -------------------------------------------- User-Card Handler -------------------------------------------- */
 
     /**
      * Handle POST request to add a card to a user's collection.
@@ -284,57 +303,115 @@ export class ApiHandler extends Storage {
      * @param {Object} res
      * @returns {null}
      */
-    UserCardPost(req,res) {
+    UserCardPost(req, res) {
         try {
-            const userCards = this.#UserCardGet(parseInt(req.params.userId),parseInt(req.params.cardId))
-            const amount = userCards.length > 0 ? userCards[0].own_amount : null
-            if(amount){
-                this.update("user_card", ["own_amount"],[amount+1],{
-                    user_id:parseInt(req.params.userId),
-                    card_id:parseInt(req.params.cardId)
-                })
-                formApiResponse(res, 200, null, "User card updated")
+            const userCards = this.#UserCardGet(
+                parseInt(req.params.userId),
+                parseInt(req.params.cardId),
+            );
+            const amount =
+                userCards.length > 0 ? userCards[0].own_amount : null;
+            if (amount) {
+                this.update("user_card", ["own_amount"], [amount + 1], {
+                    user_id: parseInt(req.params.userId),
+                    card_id: parseInt(req.params.cardId),
+                });
+                formApiResponse(res, 200, null, "User card updated");
             } else {
                 this.insert(
                     "user_card",
-                    ["user_id","card_id","own_amount"],
-                    [parseInt(req.params.userId),parseInt(req.params.cardId),1]
-                )
-                formApiResponse(res, 200, null, "User card created")
+                    ["user_id", "card_id", "own_amount"],
+                    [
+                        parseInt(req.params.userId),
+                        parseInt(req.params.cardId),
+                        1,
+                    ],
+                );
+                formApiResponse(res, 200, null, "User card created");
             }
-        } catch(err) {
-            formApiResponse(res, 500, null, err)
+        } catch (err) {
+            formApiResponse(res, 500, null, err);
         }
     }
 
     /**
-     * Handle DELETE request to get all cards for a user.
+     * Handle POST request to add a unknown card to a user's collection.
+     * @param {Object} req - The request object.
+     * @param {Object} req.params - The request parameters.
+     * @param {string} req.params.userId - The id of the user.
+     * @param {Object} res
+     * @returns {null}
+     */
+    UserCardPostUnkown(req, res) {
+        try {
+            this.update(
+                "user",
+                ["unknown_card_amount = unknown_card_amount + 1"],
+                [],
+                {
+                    id: parseInt(req.params.userId),
+                },
+            );
+        } catch (err) {
+            formApiResponse(res, 500, null, err);
+        }
+    }
+
+    /**
+     * Handle DELETE request to delete a card from a user.
      * @param {Object} req - The request object.
      * @param {Object} req.params - The request parameters.
      * @param {string} req.params.userId - The id of the user.
      * @param {Object} req.params.cardId - The id of the card.
      * @param {Object} res - The response object.
-     * @returns {null} 
+     * @returns {null}
      */
-    UserCardDelete(req,res)  { 
+    UserCardDelete(req, res) {
         try {
-            const userCards = this.#UserCardGet(parseInt(req.params.userId),parseInt(req.params.cardId))
-            const amount = userCards.length > 0 ? userCards[0].own_amount : null
-            if(amount){
-                this.update("user_card", ["own_amount"],[amount-1],{
-                    user_id:parseInt(req.params.userId),
-                    card_id:parseInt(req.params.cardId)
-                })
-                formApiResponse(res, 200, null, "User card updated")
+            const userCards = this.#UserCardGet(
+                parseInt(req.params.userId),
+                parseInt(req.params.cardId),
+            );
+            const amount =
+                userCards.length > 0 ? userCards[0].own_amount : null;
+            if (amount) {
+                this.update("user_card", ["own_amount"], [amount - 1], {
+                    user_id: parseInt(req.params.userId),
+                    card_id: parseInt(req.params.cardId),
+                });
+                formApiResponse(res, 200, null, "User card updated");
             } else {
-                this.delete("user_card",{
-                    user_id:parseInt(req.params.userId),
-                    card_id:parseInt(req.params.cardId)
-                })
-                formApiResponse(res, 200, null, "User card deleted")
+                this.delete("user_card", {
+                    user_id: parseInt(req.params.userId),
+                    card_id: parseInt(req.params.cardId),
+                });
+                formApiResponse(res, 200, null, "User card deleted");
             }
-        } catch(err) {
-            formApiResponse(res, 500, null, err)
+        } catch (err) {
+            formApiResponse(res, 500, null, err);
+        }
+    }
+
+    /**
+     * Handle DELETE request to to delete a unknown card from a user.
+     * @param {Object} req - The request object.
+     * @param {Object} req.params - The request parameters.
+     * @param {string} req.params.userId - The id of the user.
+     * @param {Object} res
+     * @returns {null}
+     */
+    UserCardDeleteUnkown(req, res) {
+        try {
+            this.update(
+                "user",
+                ["unknown_card_amount = unknown_card_amount - 1"],
+                [],
+                {
+                    id: parseInt(req.params.userId),
+                },
+            );
+        } catch (err) {
+            formApiResponse(res, 500, null, err);
         }
     }
 
@@ -344,14 +421,18 @@ export class ApiHandler extends Storage {
      * @param {number} cardId - The id of the card.
      * @returns {Object[]} - Returns an array of user cards.
      */
-    #UserCardGet = (userId,cardId) => {
+    #UserCardGet = (userId, cardId) => {
         try {
-            const where = {user_id:userId}
-            if(cardId) where.card_id = cardId
-            return this.select("user_card",["*"],where)
-        } catch(err) {
-            console.error(err)
-            return []
+            const select = ["own_amount"];
+            const where = { user_id: userId };
+
+            if (cardId) where.card_id = cardId;
+            else select.push("card_id");
+
+            return this.select("user_card", select, where);
+        } catch (err) {
+            console.error(err);
+            return [];
         }
-    }
+    };
 }
--- src/schema.sql
+++ src/schema.sql
@@ -1,8 +1,10 @@
 create table if not exists user (
     id integer primary key autoincrement,
     name text not null unique,
+    unknown_card_amount integer not null,
     discord_id text not null unique
 );
+
 create table if not exists card (
     id integer primary key autoincrement,
     title text unique not null,
@@ -13,10 +15,11 @@ create table if not exists card (
     defense integer not null,
     health integer not null
 );
+
 create table if not exists user_card (
     user_id integer not null,
     card_id integer not null,
     own_amount integer not null,
-    foreign key(user_id) references user(id),
-    foreign key(card_id) references card(id)
+    foreign key (user_id) references user (id),
+    foreign key (card_id) references card (id)
 );
