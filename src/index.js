2fd96a692a265714662f4fc0add2f51eb56dd85c
RakanMyHusbando <hellwing.jonas@web.de>
2024-12-08 14:00:26 +0100

    add unknown card post and delete routes

--- src/index.js
+++ src/index.js
@@ -1,39 +1,50 @@
-import fs from "node:fs"
-import  express  from "express"
-import dotenv from "dotenv"
-import { Storage } from "./storage.js"
-import { ApiHandler } from "./api.js"
+import fs from "node:fs";
+import express from "express";
+import dotenv from "dotenv";
+import { Storage } from "./storage.js";
+import { ApiHandler } from "./api.js";
 
-dotenv.config()
+dotenv.config();
 
 // executing schema.sql to create tables
-const db = new Storage(process.env.DB_FILE)
-db.executeSchema(fs.readFileSync("src/schema.sql").toString())
-db.close()
+const db = new Storage(process.env.DB_FILE);
+db.executeSchema(fs.readFileSync("src/schema.sql").toString());
+db.close();
 
-const apiHandler = new ApiHandler(process.env.DB_FILE)
-const app = express()
+const apiHandler = new ApiHandler(process.env.DB_FILE);
+const app = express();
 
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.listen(process.env.PORT,() => console.log(`Server running on  http://127.0.0.1:${process.env.PORT}`))
+app.listen(process.env.PORT, () =>
+    console.log(`Server running on  http://127.0.0.1:${process.env.PORT}`),
+);
 
 // Card routes
-app.post("/card", (req,res) => apiHandler.CardPost(req,res))
-app.get("/card", (req,res) => apiHandler.CardGetAll(req,res))
-app.get("/card/:id", (req,res) => apiHandler.CardGetById(req,res))
-app.patch("/card/:id", (req,res) => apiHandler.CardPatch(req,res))
-app.delete("/card/:id", (req,res) => apiHandler.CardDelete(req,res))
+app.post("/card", (req, res) => apiHandler.CardPost(req, res));
+app.get("/card", (req, res) => apiHandler.CardGetAll(req, res));
+app.get("/card/:id", (req, res) => apiHandler.CardGetById(req, res));
+app.patch("/card/:id", (req, res) => apiHandler.CardPatch(req, res));
+app.delete("/card/:id", (req, res) => apiHandler.CardDelete(req, res));
 
 // User routes
-app.post("/user", (req,res) => apiHandler.UserPost(req,res))
-app.get("/user", (req,res) => apiHandler.UserGetAll(req,res))
-app.get("/user/:id", (req,res) => apiHandler.UserGetById(req,res))
-app.patch("/user/:id", (req,res) => apiHandler.UserPatch(req,res))
-app.delete("/user/:id", (req,res) => apiHandler.UserDelete(req,res))
+app.post("/user", (req, res) => apiHandler.UserPost(req, res));
+app.get("/user", (req, res) => apiHandler.UserGetAll(req, res));
+app.get("/user/:id", (req, res) => apiHandler.UserGetById(req, res));
+app.patch("/user/:id", (req, res) => apiHandler.UserPatch(req, res));
+app.delete("/user/:id", (req, res) => apiHandler.UserDelete(req, res));
 
 // User-Card routes
-app.post("/user/:userId/card/:cardId", (req,res) => apiHandler.UserCardPost(req,res))
-app.delete("/user/:userId/card/:cardId", (req,res) => apiHandler.UserCardDelete(req,res))
-
+app.post("/user/:userId/card/:cardId", (req, res) =>
+    apiHandler.UserCardPost(req, res),
+);
+app.post("/user/:userId/card", (req, res) =>
+    apiHandler.UserCardPostUnkown(req, res),
+);
+app.delete("/user/:userId/card/:cardId", (req, res) =>
+    apiHandler.UserCardDelete(req, res),
+);
+app.delete("/user/:userId/card", (req, res) =>
+    apiHandler.UserCardDeleteUnkown(req, res),
+);
